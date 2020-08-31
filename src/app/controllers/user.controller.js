94bb70b04325668159b5b1d51b1af3ae8011c2b8
Noriuki <lanezupiroli@gmail.com>
2020-08-31 16:59:08 -0300

    ajustes controller

--- src/app/controllers/user.controller.js
+++ src/app/controllers/user.controller.js
@@ -1,40 +1,31 @@
 const User = require('../models/user.model');
-const auth = require('../services/auth.service');
+const { authenticateToken, generateToken } = require('../services/auth.service');
 
 const createUser = async (req, res) => {
     const { username, email, password, role} = req.body;
 
     try{
-      const newUser = new User({
-        username,
-        email,
-        password,
-        role
-      });
-      await newUser.save()
-      res.json("created account: " + email)
+        const newUser = new User({
+            username,
+            email,
+            password,
+            role
+        });
+        savedUser = await newUser.save()
+        res.json({
+            user: savedUser,
+            message: "create user successfully"
+        });
     } catch(err) {
-      res.json(err);
+      res.json({
+          error:err,
+          message: "create user failure"
+        });
     }
 
-      
 }
 
-const deleteUser = async (req, res) => {
 
-}
-
-const updateUser = async (req, res) => {
-
-}
-
-const searchUser = async (req, res) => {
-
-}
-
-const searchAllUsers = async (req, res) => {
-
-}
 
 module.exports = {
   createUser
